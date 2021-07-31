d71f1a9e355797d030276b4907b2060f4d7af1bb
Simon Burkhardt <simon.burkhardt.prime@gmail.com>
2021-07-31 08:08:00 +0200

    added achritecture cmake for avr32

---
+++ .CMake/toolchain_avr32.cmake
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: MIT
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR avr32)
+set(CMAKE_CROSSCOMPILING ON)
+
+set(CMAKE_C_COMPILER avr32-gcc)
+#set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-aarch64-static;-L;/usr/aarch64-linux-gnu/")
