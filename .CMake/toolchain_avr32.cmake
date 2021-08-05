c9b7e1f678bcb28e296d768c92c2cacd8bff4cc7
Simon Burkhardt <simon@simonmartin.ch>
2021-08-05 08:59:16 +0200

    avr32-gcc is broken on WSL, switching to VM

--- .CMake/toolchain_avr32.cmake
+++ .CMake/toolchain_avr32.cmake
@@ -1,12 +1,17 @@
 # SPDX-License-Identifier: MIT
 
-set(CMAKE_SYSTEM_NAME avr32)
-set(CMAKE_SYSTEM_PROCESSOR avr32)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR avr)
 set(CMAKE_CROSSCOMPILING ON)
 
 set(CMAKE_C_COMPILER avr32-gcc)
 set(CMAKE_CXX_COMPILER avr32-g++)
 set(CMAKE_ASM_COMPILER avr32-gcc)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
-set(CMAKE_COMPILER_WORKS)
+
+#set(CMAKE_C_COMPILER_WORKS 1) # force override initial compiler test
+#add_compile_options(--specs=nano.specs)
+
+
+
 #set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-aarch64-static;-L;/usr/aarch64-linux-gnu/")
